5b0f6a8fa71036456059de1aaad95d28261e883d
MaxX <maxx-f@mail.ru>
2022-08-30 08:34:30 +0300

    Add DZ_001

---
+++ DZ_001/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int m = 4;
+int n = 8;
+int range = 100;
+int[,] array = new int[m, n];
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($"{array[i, j]}\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+void SortArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+Console.WriteLine($"\nЗаданый массив: ");
+CreateArray(array);
+PrintArray(array);
+
+Console.WriteLine($"\nОтсортированный массив: ");
+SortArrayLines(array);
+PrintArray(array);
